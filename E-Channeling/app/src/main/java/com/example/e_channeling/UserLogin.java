4070a784f35013a19ea9960633d171511d39be9d
shalithacode <shalithalakruwan1998@gmail.com>
2021-10-05 22:57:02 +0530

    Register ui created

--- E-Channeling/app/src/main/java/com/example/e_channeling/UserLogin.java
+++ E-Channeling/app/src/main/java/com/example/e_channeling/UserLogin.java
@@ -2,13 +2,29 @@ package com.example.e_channeling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class UserLogin extends AppCompatActivity {
+public class UserLogin extends AppCompatActivity implements View.OnClickListener{
 
+    private TextView register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+        register =findViewById(R.id.txtToRegister);
+        register.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case (R.id.txtToRegister):
+                startActivity(new Intent(this, UserRegister.class));
+                break;
+
+
+        }
     }
 }
