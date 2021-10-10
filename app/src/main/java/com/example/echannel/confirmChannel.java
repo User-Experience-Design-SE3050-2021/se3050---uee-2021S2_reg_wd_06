b8146a8931892c8f5a1e31a1f2b27894573352b5
Tharushi N Dewangi <dewsdrv@gmail.com>
2021-10-10 20:52:43 +0530

    payment Added

--- app/src/main/java/com/example/echannel/confirmChannel.java
+++ app/src/main/java/com/example/echannel/confirmChannel.java
@@ -2,13 +2,33 @@ package com.example.echannel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class confirmChannel extends AppCompatActivity {
 
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_channel);
+        b1=findViewById(R.id.button8);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnback();
+            }
+        });
+    }
+    public void returnback() {
+        //Toast.makeText(getApplicationContext(), "Successfully added", Toast.LENGTH_SHORT).show();
+
+        //call second activity
+        Intent intent = new Intent(this, makePayment.class);
+        startActivity(intent);
+
     }
 }
