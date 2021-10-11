1528c24df30b448183a3a652302d2796f7e170ef
DilmiMinoli <dminoli98@gmail.com>
2021-10-11 08:27:44 +0530

    driving license medical

---
+++ app/src/main/java/com/example/echannel/confirmChannel.java
@@ -0,0 +1,14 @@
+package com.example.echannel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class confirmChannel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_channel);
+    }
+}
