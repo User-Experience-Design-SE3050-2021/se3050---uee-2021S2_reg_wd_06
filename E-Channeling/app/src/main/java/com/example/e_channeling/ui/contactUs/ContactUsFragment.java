3c741ebb096b973ce2500c20f815d85a55c1696c
shalithacode <shalithalakruwan1998@gmail.com>
2021-10-11 06:54:40 +0530

    Contact Us implemented

--- E-Channeling/app/src/main/java/com/example/e_channeling/ui/contactUs/ContactUsFragment.java
+++ E-Channeling/app/src/main/java/com/example/e_channeling/ui/contactUs/ContactUsFragment.java
@@ -1,10 +1,13 @@
 package com.example.e_channeling.ui.contactUs;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.BinderThread;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
@@ -19,10 +22,34 @@ public class ContactUsFragment extends Fragment {
         binding = FragmentContactUsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        binding.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:9477778888"));
+                startActivity(callIntent);
+            }
+        });
+        binding.webBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.echannelling.com/Echannelling/";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        binding.emailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, "");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "");
 
-
-
-
+                startActivity(intent);
+            }
+            });
 
         return root;
     }
